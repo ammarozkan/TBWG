bc634f79b15216b4716633ce48dda23f582d3b8b
ammarozkan <61326832+ammarozkan@users.noreply.github.com>
2025-06-07 08:47:11 +0300

    changed to morsels

--- TheGreatFighters/mods/STREETFIGHTER/STREETFIGHTER.h
+++
@@ -1,9 +0,0 @@
-#define TGF_FIGHTER_EVENTER_INDICE 10
-
-#define TGF_STREETFIGHTER_PUSH_NAME "TGF_STFGHT_PUSH"
-#define TGF_STREETFIGHTER_PUSH (TGF_FIGHTER_EVENTER_INDICE+0)
-
-#define TGF_STREETFIGHTER_CHARACTERCODE 2
-
-#include "push.h" // getTgfPush
-#include "character.h" // tgfGetStreetFighter
--- TheGreatFighters/mods/TONSOFEFFECTS/TONSOFEFFECTS.h
+++
@@ -1,8 +0,0 @@
-// independent effect library mod
-// getTGFPuzzledEffect
-
-#define TGF_TONSOFEFFECTS_EFFECTS_INDICE 16
-
-#define TGF_TONSOFEFFECTS_PUZZLED (TGF_TONSOFEFFECTS_EFFECTS_INDICE+0)
-
-#include "effect_puzzled.h"
