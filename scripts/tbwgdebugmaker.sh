e5696f7e088ccbef26acece908ecf26588267e34
ammarozkan <61326832+ammarozkan@users.noreply.github.com>
2025-09-25 11:43:12 +0300

    Delete scripts directory

--- scripts/tbwgdebugmaker.sh
+++
@@ -1,7 +0,0 @@
-git clone https://github.com/ammarozkan/TBWG.git
-cd TBWG
-cd TBWGLib
-make tbwgessentialslib debug
-cd ..
-make
-make debugserverdemo
--- scripts/tbwgmaker.sh
+++
@@ -1,7 +0,0 @@
-git clone https://github.com/ammarozkan/TBWG.git
-cd TBWG
-cd TBWGLib
-make tbwgessentialslib lib
-cd ..
-make
-make serverdemo
