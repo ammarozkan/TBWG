8a2c9f308d0b5f427634312acdd52778479f1012
ammarozkan <61326832+ammarozkan@users.noreply.github.com>
2026-02-12 22:56:57 +0300

    more debugging

--- TBWGDevelopingFramework/TBWGServerTemplate/MakeWindows.bat
+++ TBWGDevelopingFramework/TBWGServerTemplate/MakeWindows.bat
@@ -1,5 +1,5 @@
 del ..\bin\tbwgserver.exe
-gcc main.c -I ../TBWGLib/include/ -L ../TBWGLib/libs/ -ltbwgdebug -ltbwgnetdebug -ltbwgessentials -lws2_32 -lm -o ../bin/tbwgserver
+gcc -Wno-incompatible-pointer-types main.c -I ../TBWGLib/include/ -L ../TBWGLib/libs/ -ltbwgdebug -ltbwgnetdebug -ltbwgessentials -lws2_32 -lm -o ../bin/tbwgserver
 echo OK
 pause
 .\..\bin\tbwgserver.exe --rounds 1024
